5a0ea09cdaf2c3c81b41bf77b37ae67d5b9937b0
Tom Atwell <atwell@chapman.edu>
2017-07-12 15:55:14 -0700

    Updates index.html to include Bootstrap.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   entry: __dirname + '/js/index.js',
   output: {
     path: __dirname + '/build',
-    filename: 'bundle.js'
+    filename: 'omninav.js'
   },
   module: {
     rules: [{
