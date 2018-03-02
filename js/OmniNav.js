3652383eccc005f3a0bdf9755463dc93c12134c2
Kayla Ziegler <kziegler@chapman.edu>
2018-03-01 16:11:58 -0800

    beginning of primary nav html with off canvas icon and primary logo

--- js/OmniNav.js
+++ js/OmniNav.js
@@ -25,20 +25,53 @@ var OmniNav = (function() {
 
   var buildNav = function(target) {
     console.log("Building OmniNav for target: " + target);
-    var $omniNav = $('<nav id="omni-nav" class="post-build" />');
-    var $logo = buildLogoLink();
-    $omniNav.append($logo);
+    var $omniNav = $('#omni-nav-v2');
+    var $primary = buildPrimaryNav();
+    $omniNav.append($primary);
     return $omniNav;
   };
 
   var buildLogoLink = function() {
-    var $a = $('<a />').attr('id', 'omni-nav-logo')
-                       .attr('href', HOME_PAGE_URL)
-                       .attr('title', 'Chapman University Website Home Page');
+    var $a = $('<a />').attr({
+        class: 'primary-logo',
+        href: HOME_PAGE_URL,
+        title: 'Chapman University Website Home Page'
+    });
     var $img = $('<img />').attr('src', LOGO_URL);
     $a.append($img);
     return $a;
   };
+
+  var buildPrimaryNav = function() {
+    var $primary = $('<div />').attr('id', 'primary-nav');
+    var $offCanvas = $('<div />').attr('class', 'nav-container left-nav-container');
+    var $offCanvasTrigger = $('<a />').attr({
+        class: 'off-canvas-trigger primary-nav-icon',
+        href: '#',
+        id: 'js-off-canvas-trigger',
+        role: 'button',
+        title: 'Access links to the pages within this section of the website and to other sections of the website',
+        'aria-label': 'Access links to the pages within this section of the website and to other sections of the website'
+    });
+
+    var $offCanvasIcon = buildOffCanvasSVG();
+    $offCanvasTrigger.append($offCanvasIcon);
+    $offCanvas.append($offCanvasTrigger);
+    var $logo = buildLogoLink();
+    $primary.append($offCanvas, $logo);
+    return $primary;
+  }
+
+  var buildOffCanvasSVG = function() {
+    // Using document.createElementNS("http://www.w3.org/2000/svg", "svg") didn't work here to create inline SVGs
+    var $paths = '<path d="M1 3h14v2h-14v-2z"></path><path d="M1 7h14v2h-14v-2z"></path><path d="M1 11h14v2h-14v-2z"></path>';
+    var $svg = '<svg xmlns="http://www.w3.org/2000/svg" class="hamburger-icon" viewBox="0 0 16 16">' + $paths + '</svg>';
+    return $svg;
+  }
+
+  var buildChapmanLogoSVG = function() {
+
+  }
 
   // Public API
   return {
